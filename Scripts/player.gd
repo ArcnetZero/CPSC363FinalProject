838fae665958dccacf84b8ba2212d0aa4a0d21e5
ericflyr <33262035+ericflyr@users.noreply.github.com>
2017-12-05 13:25:04 -0600

    catchup
    asdf

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -28,14 +28,12 @@ const MAX_FALL_SPEED = 1400
 var jump_count = 0
 const MAX_JUMP_COUNT = 1
 
-var is_on_ground = true
 var wait_time = 0
 
 func _ready():
 	set_process(true)
 	set_process_input(true)
 	sprite_node = get_node("Sprite")
-
 
 func _input(event):
 	if is_small == 0 and event.is_action_pressed("change_character_right"):
@@ -50,11 +48,80 @@ func _input(event):
 		else:
 			curr_character = 3
 		change_sprite(curr_character)
-	if jump_count < MAX_JUMP_COUNT and event.is_action_pressed("jump"):
-		speed.y = -JUMP_FORCE
-		jump_count += 1
 	if curr_character == 2 and event.is_action_pressed("use_ability"):
 		change_size()
+
+func _process(delta):
+	if wait_time == 0:
+		if input_direction:
+			direction = input_direction
+			
+		if Input.is_action_pressed("move_left"):
+			input_direction = -1
+			sprite_flipper(input_direction)
+		elif Input.is_action_pressed("move_right"):
+			input_direction = 1
+			sprite_flipper(input_direction)
+		else:
+			input_direction = 0
+			
+		if input_direction == -direction:
+			speed.x = 0
+		if input_direction:
+			speed.x += ACCELERATION * delta
+		else:
+			speed.x -= DECELERATION * delta
+		speed.x = clamp(speed.x, 0, MAX_SPEED)
+		
+		speed.y += (GRAVITY * delta)* 2
+		if speed.y > MAX_FALL_SPEED:
+			speed.y = MAX_FALL_SPEED
+			
+		velocity = Vector2(speed.x * delta * direction, speed.y * delta)
+		var movement_remainder = move(velocity)
+		
+		
+		
+		if is_colliding():
+			var normal = get_collision_normal()
+			var final_movement = normal.slide(movement_remainder)
+			speed = normal.slide(speed)
+			move(final_movement)
+			
+			if normal != Vector2(0, 1):
+				jump_count = 0
+			
+			if normal == Vector2(0,-1) and Input.is_action_pressed("jump") and jump_count < MAX_JUMP_COUNT:
+				speed.y = -JUMP_FORCE
+				jump_count +=1
+			elif (normal == Vector2(-1,0) or normal == Vector2(1,0)) and Input.is_action_pressed("jump") and jump_count < MAX_JUMP_COUNT:
+				speed.y = -JUMP_FORCE
+				speed.x += ACCELERATION * delta
+				velocity = Vector2(speed.x * delta * -direction, speed.y * delta)
+				var movement_remainder = move(velocity)
+				sprite_flipper(-direction)
+				wait_time = 13
+				
+			
+	elif wait_time > 0:
+		speed.x += (ACCELERATION * delta)* 3
+		speed.x = clamp(speed.x,0,MAX_SPEED)
+		speed.y += (GRAVITY * delta)*2
+		if speed.y > MAX_FALL_SPEED:
+			speed.y = MAX_FALL_SPEED
+		velocity = Vector2(speed.x * delta * -direction, speed.y * delta)
+		var movement_remainder = move(velocity)
+		wait_time -= 1
+
+func sprite_flipper(dir):
+	if dir == -1:
+		sprite_node.set_flip_h(true)
+		sprite1_node.set_flip_h(true)
+		sprite2_node.set_flip_h(true)
+	elif dir == 1:
+		sprite_node.set_flip_h(false)
+		sprite1_node.set_flip_h(false)
+		sprite2_node.set_flip_h(false)
 
 func change_sprite(num):
 	if num == 1:
@@ -77,63 +144,3 @@ func change_size():
 	elif is_small == 1:
 		is_small = 0
 		self.scale(Vector2(2, 2))
-
-func sprite_flipper(dir):
-	if dir == -1:
-		sprite_node.set_flip_h(true)
-		sprite1_node.set_flip_h(true)
-		sprite2_node.set_flip_h(true)
-	elif dir == 1:
-		sprite_node.set_flip_h(false)
-		sprite1_node.set_flip_h(false)
-		sprite2_node.set_flip_h(false)
-
-func _process(delta):
-	if wait_time > 0:
-		wait_time -= 1
-	else:
-		if input_direction:
-			direction = input_direction
-			
-		if Input.is_action_pressed("move_left") and is_on_ground:
-			input_direction = -1
-			sprite_flipper(input_direction)
-		elif Input.is_action_pressed("move_right") and is_on_ground:
-			input_direction = 1
-			sprite_flipper(input_direction)
-		else:
-			input_direction = 0
-			
-		if input_direction == -direction:
-			speed.x /= 10
-		if input_direction:
-			speed.x += ACCELERATION * delta
-		else:
-			speed.x -= DECELERATION * delta
-		speed.x = clamp(speed.x, 0, MAX_SPEED)
-		
-		speed.y += GRAVITY * delta
-		if speed.y > MAX_FALL_SPEED:
-			speed.y = MAX_FALL_SPEED
-			
-		velocity = Vector2(speed.x * delta * direction, speed.y * delta)
-		var movement_remainder = move(velocity)
-		
-		if is_colliding():
-			var normal = get_collision_normal()
-			var final_movement = normal.slide(movement_remainder)
-			speed = normal.slide(speed)
-			move(final_movement)
-			
-			if normal == Vector2(0, -1):
-				jump_count = 0
-			elif normal == Vector2(1,0) or normal == Vector2(-1,0):
-				speed.y /= 2
-				if Input.is_action_pressed("jump"):
-					wait_time = 4
-					speed.x = MAX_SPEED * 4
-					speed.y = (-JUMP_FORCE / 2)
-					direction = -direction
-					velocity = Vector2(speed.x * delta * direction, speed.y * delta)
-					sprite_flipper(direction)
-					move(velocity)
