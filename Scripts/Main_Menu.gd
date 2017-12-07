7f139db13c09c94e1647911d56715235127e55ab
ericflyr <33262035+ericflyr@users.noreply.github.com>
2017-12-07 13:52:55 -0600

    splash
    adf

--- Scripts/Main_Menu.gd
+++ Scripts/Main_Menu.gd
@@ -1,4 +1,10 @@
 extends TextureFrame
 
 func _ready():
-	pass
+	set_process_input(true)
+	
+	
+func _input(event):
+	if event.is_action_pressed("jump"):
+		get_tree().change_scene("res://Scenes/Level1.tscn")
+		
